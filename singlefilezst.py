68e55cdd6274be6d553536b4cdcd3177c5be7fa1
munggok <samsulrahmadani@gmail.com>
2022-08-27 17:08:33 +0000

    add zstd singlefile

--- singlefilezst.py
+++ singlefilezst.py
@@ -2,15 +2,19 @@ import sys
 import jsonlines
 import gzip
 import json
+import zstandard as zstd
 from clean import clean_text
 
+params = zstd.ZstdCompressionParameters.from_level(12)
+cctx = zstd.ZstdCompressor(compression_params=params)
 
-PATH ="/data/mc4_cleanedv2/"
 
+PATH ="/data/2021_49_raw_cleaned/"
 def main(file):
     file_name = file.split('/')[-1]
-    with gzip.open(f"{PATH}/cleaned_{file_name}", mode='wb') as writer:
+    with zstd.open(f"{PATH}/cleaned_{file_name}",cctx=cctx,encoding="utf-8", mode='wb') as writer:
         with gzip.open(f'{file}') as f:
+        #with zstd.open(open(f'{file}', "rb"), "rt", encoding="utf-8") as f:
             for line in f:
                 obj = json.loads(line)
                 cleaned = clean_text(obj['text'])
@@ -22,4 +26,3 @@ def main(file):
 if __name__ == '__main__':
     file = sys.argv[1]
     main(file)
-
